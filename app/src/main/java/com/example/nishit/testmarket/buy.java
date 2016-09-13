4dd0052df03a5d8a3ca1d50d44db6dbf66e436f0
Nishit Sakariya <ns11251@gmail.com>
2016-09-13 20:37:45 +0530

    First multiscreen android app

---
+++ app/src/main/java/com/example/nishit/testmarket/buy.java
@@ -0,0 +1,19 @@
+package com.example.nishit.testmarket;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class buy extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_buy);
+    }
+
+    public void func(View v)
+    {
+        finish();
+    }
+}
